878dbc7445131100d681349e278f5355860758ee
jaimeforian27 <jaimeflorian27@gmail.com>
2021-11-27 22:29:16 -0800

    UI finished, Open Maya used for transformations

--- joint_Inbetweener.py
+++ joint_Inbetweener.py
@@ -2,10 +2,22 @@
 #Created by Jaime Florian1
 
 import maya.cmds as cmds
+import maya.api.OpenMaya as om2
 from functools import partial
 
 def getSliderValue(slider):
     return cmds.floatSliderGrp(slider,q=1, v=1)
+
+def setJointPosition(start_jnt,inbtwn_jnt,end_jnt,slider):
+     w = getSliderValue(slider)
+     print(w)
+     start_jnt_v = om2.MVector(cmds.xform(start_jnt, q=True, t=True, ws=True))
+     end_jnt_v = om2.MVector(cmds.xform(end_jnt, q=True, t=True,ws=True))
+     inbtwn_jnt_v = (end_jnt_v-start_jnt_v) * w + start_jnt_v
+     print(inbtwn_jnt_v)
+     cmds.xform(inbtwn_jnt, t=list(inbtwn_jnt_v), ws= True)
+     cmds.parent(end_jnt,inbtwn_jnt)
+     cmds.select(inbtwn_jnt)
 
 def insertMiddleJoint(slider, *args):
 
@@ -32,10 +44,7 @@ def insertMiddleJoint(slider, *args):
             radius = (cmds.getAttr("%s.radius" % joints[0]) + cmds.getAttr("%s.radius" % end_jnt) )/2
             #create the inbetween joint (it is created as a children of the parent joint because it is still selected, it's one of the properties of the cmds.joint command)
             inbetween_jnt= cmds.joint(rad= radius)
-            #create a parent constraint between the start, end and middle joint and delete it
-            cmds.delete(cmds.parentConstraint(joints[0], end_jnt, inbetween_jnt))
-            #set the middle joint as the parent of the final joint
-            cmds.parent(end_jnt,inbetween_jnt)
+            setJointPosition(joints[0],inbetween_jnt,end_jnt,slider)
 
     #if two joints are selected
     if len(joints) == 2:
@@ -53,8 +62,7 @@ def insertMiddleJoint(slider, *args):
                 end_jnt = joints[1]
                 radius = (cmds.getAttr("%s.radius" % joints[0]) + cmds.getAttr("%s.radius" % end_jnt) )/2
                 inbetween_jnt= cmds.joint(rad= radius)
-                cmds.delete(cmds.parentConstraint(joints[0], end_jnt, inbetween_jnt))
-                cmds.parent(end_jnt,inbetween_jnt)
+                setJointPosition(joints[0],inbetween_jnt,end_jnt,slider)
         else:
             cmds.warning("First joint must be a direct parent of the second joint")
 
@@ -62,11 +70,19 @@ def insertMiddleJoint(slider, *args):
     if len(joints) > 2:
         cmds.warning("Select only one or two joints")
 
+    cmds.setFocus("MayaWindow")
+
+
 #create window
-mainWindow = cmds.window(t = "Joint Inbetweener", wh=(200,100), mxb=False)
+mainWindow = cmds.window(t = "Joint Inbetweener", wh=(200,140), mxb=False)
 layout = cmds.columnLayout(adjustableColumn=True, rs = 10)
 separator_top = cmds.separator(st='none')
 startEnd_label = cmds.text('Start-End position:', al='left')
-positionFloatSlider = cmds.floatSliderGrp(min=-1.0, max=1.0, v=0.0, field = True)
+positionFloatSlider = cmds.floatSliderGrp(min=-0.0, max=1.0, v=0.5, field = True)
 insertButton = cmds.button(l = "Insert Joint", al = 'center', c= partial(insertMiddleJoint, positionFloatSlider))
+cmds.setParent('..')
+layout2 = cmds.rowLayout(nc=2, adj= True)
+jointNumber_intField= cmds.intField(min = 2, max = 100, v=2)
+insertMultiple_btn = cmds.button(label = 'Insert Joints')
+
 cmds.showWindow(mainWindow)
